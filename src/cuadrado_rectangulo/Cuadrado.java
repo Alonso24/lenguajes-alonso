3a401056d17c103b8cf662f5188629e85affac29
ALGZ <ALGZ@ALGZ-PC>
2015-02-09 01:55:46 -0600

    CalculoAreas

---
+++ src/cuadrado_rectangulo/Cuadrado.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuadrado_rectangulo;
+
+/**
+ *
+ * @author ALGZ
+ */
+public class Cuadrado {
+   private int lado;
+   
+   
+   public void setLado(int lado){
+   this.lado=lado;
+   }
+   public int getLado(){
+   return lado;
+   }
+   
+public int calcularArea(){
+
+int area = lado * lado;
+
+return area;
+}
+    
+}
