69e88b9e61ef9b45ae374699c50247852f125417
ALGZ <ALGZ@ALGZ-PC>
2015-02-09 01:44:43 -0600

    CalculoAreas

--- src/cuadrado_rectangulo/AplicacionCuadrado.java
+++ src/cuadrado_rectangulo/AplicacionCuadrado.java
@@ -10,15 +10,15 @@ public class AplicacionCuadrado {
     
      public static void main(String[] args) {
     
-      Cuadrado c=new Cuadrado();
+      //Cuadrado c=new Cuadrado();
       Rectangulo r=new Rectangulo();
      
-        c.setLado(4);
+       // c.setLado(4);
         r.setLado1(3);
         r.setLado2(5);
         
       
-         System.out.println(c.calcularArea());
+         //System.out.println(c.calcularArea());
          System.out.println(r.calcularArea());
      }
 }
