79960d5c5ffd1bd3072c38b3e73572bd49d93841
ALGZ <ALGZ@ALGZ-PC>
2015-02-09 01:36:06 -0600

    prueba

---
+++ src/prueba/MainTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author ALGZ
+ */
+public class MainTest {
+    
+     public static void main(String[] args){
+         
+       
+         Rectangulo x = new Rectangulo (2);
+       
+       /*x.visualizate();
+       
+       Rectangulo y = newRectangulo();
+               
+               
+     System.out.println(x.getladoMayor());
+     */
+         
+         System.out.println(x.ladoMayor);
+     }
+    
+}
---
+++ src/prueba/Rectangulo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author ALGZ
+ */
+public class Rectangulo {
+    
+   int ladoMayor;
+   /* int ladoMenor;
+    
+    public Rectangulo(int ladoMayor,int ladoMenor){
+        this.ladoMayor = ladoMayor;
+        this.ladoMenor = ladoMenor;
+    }
+    
+    
+    public Rectangulo(){
+        ladoMayor = 2 ;
+    
+    */
+    public Rectangulo (int ladoMayor){
+    
+        this.ladoMayor=ladoMayor;
+        
+    }
+    public Rectangulo(){
+    
+        ladoMayor = 2 ;
+        
+    }
+}
