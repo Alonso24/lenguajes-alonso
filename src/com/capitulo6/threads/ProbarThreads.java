fcb7f5ed1723e49056a185dc667d6925a6e89c5e
T-301 <T-301@PC333>
2015-03-12 19:41:30 -0600

    Threads

---
+++ src/com/capitulo6/threads/ProbarThreads.java
@@ -0,0 +1,44 @@
+
+package com.capitulo6.threads;
+
+/**
+ *
+ * @author T-301
+ */
+public class ProbarThreads {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Probar Threads");
+        //Paso 1: Crearlo 
+        Facil t1=new Facil();
+        Facil t11=new Facil();
+        Reloj treloj=new Reloj();
+        Regular r1=new Regular(); //r es de runnable
+        Thread t2=new Thread(r1);
+        Thread t3=new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                System.out.println("Soy un thread mucho mejor");
+            }
+            
+        }
+        );//constructor clase thrad
+        
+        
+        
+         //t1.setName("Petra");
+        //Paso 2: Inicializar
+        t1.start();
+        t11.start();
+        t2.start();
+        t3.start();
+        treloj.start();
+        
+       
+                 }
+    
+}
